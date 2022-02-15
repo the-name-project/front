4fecb1a067f0cd758b0e1349147ab5a07a9038f9
coupszi <peniandfub@daum.com>
2022-02-15 22:27:10 +0900

    +master

---
+++ src/pages/Join/StyledJoin.jsx
@@ -0,0 +1,141 @@
+import styled, { css } from "styled-components";
+
+export const LogoImg = styled.img`
+    position: fixed;
+    left: 20px;
+    top: 20px;
+    background-color: #FEE9B2;
+`;
+export const MypageLink = styled.p`
+    position: fixed;
+    right: 20px;
+    top: 10px;
+`;
+
+export const Title = styled.h2`
+    font-size: 50px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-top: 3%;
+`;
+
+export const JoinForm = styled.form`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-bottom: 50px;
+`;
+export const Form = styled.div`
+    width: 650px;
+`;
+
+export const NAME = styled.div`
+    margin: 1% 0;
+`;
+export const NameTitle = styled.label`
+    color: #9e9e9e;
+`;
+export const NameForm = styled.input`
+    width: 650px;
+    height: 40px;
+    background-color: #FEE9B2;
+    border: none;
+    border-radius: 10px;
+`;
+
+export const GO = styled.div`
+    margin: 1% 0;
+`;
+export const GoTitle = styled.label`
+    color: #9e9e9e;
+`;
+export const GoForm = styled.input`
+    width: 650px;
+    height: 40px;
+    background-color: #FEE9B2;
+    border: none;
+    border-radius: 10px;
+`;
+
+export const ID = styled.div`
+    margin: 1% 0;
+`;
+export const IdTitle = styled.label`
+    color: #9e9e9e;
+`;
+export const IdForm = styled.input`
+    width: 650px;
+    height: 40px;
+    background-color: #FEE9B2;
+    border: none;
+    border-radius: 10px;
+`;
+
+export const PW = styled.div`
+    margin: 1% 0;
+`;
+export const PwTitle = styled.label`
+    color: #9e9e9e;
+`;
+export const PwForm = styled.input`
+    width: 650px;
+    height: 40px;
+    background-color: #FEE9B2;
+    border: none;
+    border-radius: 10px;
+`;
+
+export const PWMore = styled.div`
+    margin: 1% 0;
+`;
+export const PwMoreTitle = styled.label`
+    color: #9e9e9e;
+`;
+export const PwMoreForm = styled.input`
+    width: 650px;
+    height: 40px;
+    background-color: #FEE9B2;
+    border: none;
+    border-radius: 10px;
+`;
+
+export const Question = styled.div`
+    margin: 1% 0;
+`;
+export const QuesTitle = styled.label`
+    color: #9e9e9e;
+`;
+export const QuesForm = styled.select`
+    width: 650px;
+    height: 40px;
+    background-color: #FEE9B2;
+    border: none;
+    border-radius: 10px;
+`;
+export const Q = styled.option`
+    font-size: 15px
+`;
+
+export const Answer = styled.div`
+    margin: 1% 0;
+`;
+export const AnsTitle =styled.label`
+    color: #9e9e9e;
+`;
+export const AnsForm =styled.input`
+    width: 650px;
+    height: 40px;
+    background-color: #FEE9B2;
+    border: none;
+    border-radius: 10px;
+`;
+
+export const SbForm = styled.input`
+    width: 650px;
+    height: 40px;
+    background-color: #FEE9B2;
+    border: none;
+    border-radius: 10px;
+    margin: 3% 0;
+`;
---
+++ src/pages/Login/StyledLogin.jsx
@@ -0,0 +1,84 @@
+import styled, { css } from "styled-components";
+
+export const LogoImg = styled.img`
+    position: fixed;
+    left: 20px;
+    top: 20px;
+    background-color: #FEE9B2;
+`;
+export const MypageLink = styled.p`
+    position: fixed;
+    right: 20px;
+    top: 10px;
+`;
+
+export const Title = styled.h2`
+    font-size: 50px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-top: 8%;
+`;
+
+export const LoginForm = styled.form`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-bottom: 50px;
+`;
+export const Form = styled.div`
+    width: 650px;
+`;
+export const ID = styled.div`
+    margin: 3% 0;
+`;
+export const IdTitle = styled.label`
+    color: #9e9e9e;
+`;
+export const IdForm = styled.input`
+    width: 650px;
+    height: 40px;
+    background-color: #FEE9B2;
+    border: none;
+    border-radius: 10px;
+`;
+
+export const PW = styled.div`
+    margin: 3% 0;
+`;
+export const PwTitle = styled.label`
+    color: #9e9e9e;
+`;
+export const PwForm = styled.input`
+    width: 650px;
+    height: 40px;
+    background-color: #FEE9B2;
+    border: none;
+    border-radius: 10px;
+`;
+export const SbForm = styled.input`
+    width: 657px;
+    height: 40px;
+    margin: 2% 0;
+    background-color: #FEE9B2;
+    border: none;
+    border-radius: 10px;
+    font-size: 0.8 rem;
+`;
+
+export const Surch = styled.div`
+    position: relative;
+    left: 450px;
+`;
+export const SurchId = styled.h6`
+    display: inline-block;
+    margin: 0 1%;
+`;
+export const SurchPw = styled.h6`
+    display: inline-block;
+    margin: 0 1%;
+`;
+export const DoJoin = styled.h6`
+    display: inline-block;
+    margin: 0 1%;
+`;
