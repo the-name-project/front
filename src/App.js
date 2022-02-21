44fbed5610cb06ab9ba4c9a895728ba400f14a0c
limmisong <althd2693@gmail.com>
2022-02-21 17:50:06 +0900

    Mypage_

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import Store from "./pages/Store/Store";
 import Main from "./pages/Main/Main";
+import Mypage from "./pages/Mypage/Mypage";
 
 function App() {
   return (
     <Router>
-      <Routes>
-        {/* <Route path="/" element={<Store />} /> */}
+      <Routes>z
+        <Route path = "/" element = {<Mypage/>}/>
+        <Route path="/" element={<Store />} />
         <Route path="/" element={<Main />} />
       </Routes>
     </Router>
