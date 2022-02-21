cafce682e51af1f96cfc10139c0a5f81c1a3c5d7
LeeJaeHoon <dlwogns3413@naver.com>
2022-02-22 01:05:11 +0900

    fix: conflic resolve

--- src/pages/Mypage/Mypage.jsx
+++ src/pages/Mypage/Mypage.jsx
@@ -1,79 +1,74 @@
 import React from "react";
-import {Test, MypageCon, MypageTop, LogoImg, 
-    MypageUserInfo, MypagePro1, MypagePro2, 
-    MypageProNickname, ChangeBtn, MypageProEtc, 
-    ProfileImg, MypageMenu, MypageMenuN, 
-    MypageJjimStore, MypageJjimLine, MypageJjimUnit
-} from './StyledMypage';
-<<<<<<< HEAD
+import {
+  Test,
+  MypageCon,
+  MypageTop,
+  LogoImg,
+  MypageUserInfo,
+  MypagePro1,
+  MypagePro2,
+  MypageProNickname,
+  ChangeBtn,
+  MypageProEtc,
+  ProfileImg,
+  MypageMenu,
+  MypageMenuN,
+  MypageJjimStore,
+  MypageJjimLine,
+  MypageJjimUnit,
+} from "./StyledMypage";
+
 import logo from "./만원의행복3.png";
 import defaultProfile from "./defaultProfile.png";
-=======
->>>>>>> 89947117bc2f6de293f9c09c6a14b9d6d9adb869
 
-const Mypage=()=>{
-    return(
-        <div>
-<<<<<<< HEAD
-            <MypageCon>s
-{/* ---------------1---------------------------- */}
-                <MypageTop>
-                    <LogoImg src = {logo} alt = "로고_사진"/>
-=======
-            <MypageCon>
-{/* ---------------1---------------------------- */}
-                <MypageTop>
-                    <LogoImg src = "../../LOGO/만원의행복3.png"/>
->>>>>>> 89947117bc2f6de293f9c09c6a14b9d6d9adb869
-                </MypageTop>
-{/* ---------------2---------------------------- */}
-                <MypageUserInfo>
-                    <MypagePro1>
-<<<<<<< HEAD
-                        <ProfileImg src = {defaultProfile}
-                        alt = "기본프로필사진"
-                        width = "100%" height = "98%"/>
-=======
-                        <ProfileImg src = "../../img/default_profile.png"/>
->>>>>>> 89947117bc2f6de293f9c09c6a14b9d6d9adb869
-                    </MypagePro1>
-                    <MypagePro2>
-                        <MypageProNickname>
-                            상큼한 양말
-                            <ChangeBtn>정보수정</ChangeBtn>
-                        </MypageProNickname>
-                        <MypageProEtc>
-                            임미송 / 여 / 0000@naver.com
-                        </MypageProEtc>
-                    </MypagePro2>
-                </MypageUserInfo>
-{/* ---------------3---------------------------- */}
-                <MypageMenu>
-                    <MypageMenuN>찜</MypageMenuN>
-                    <MypageMenuN>리뷰</MypageMenuN>
-                    <MypageMenuN>투표</MypageMenuN>
-                </MypageMenu>
-{/* ---------------4---------------------------- */}
-                <MypageJjimStore>
-                    <MypageJjimLine>
-                        <MypageJjimUnit src = "..\img\옥반1.jpg"/>
-                        <MypageJjimUnit src = "../img/음식2.jpg"/>
-                        <MypageJjimUnit src = "img/음식3.jpg"/>
-                    </MypageJjimLine>
-                    <MypageJjimLine>
-                        <MypageJjimUnit src = "img/음식4.jpg"/>
-                        <MypageJjimUnit src = "img/음식5.jpg"/>
-                        <MypageJjimUnit src = "img/음식6.jpg"/>
-                    </MypageJjimLine>
-                </MypageJjimStore>
-
-
-            </MypageCon>
-
-
-
-        </div>
-    );
+const Mypage = () => {
+  return (
+    <div>
+      <MypageCon>
+        {/* ---------------1---------------------------- */}
+        <MypageTop>
+          <LogoImg src={logo} alt="로고_사진" />
+        </MypageTop>
+        {/* ---------------2---------------------------- */}
+        <MypageUserInfo>
+          <MypagePro1>
+            <ProfileImg
+              src={defaultProfile}
+              alt="기본프로필사진"
+              width="100%"
+              height="98%"
+            />
+          </MypagePro1>
+          <MypagePro2>
+            <MypageProNickname>
+              상큼한 양말
+              <ChangeBtn>정보수정</ChangeBtn>
+            </MypageProNickname>
+            <MypageProEtc>임미송 / 여 / 0000@naver.com</MypageProEtc>
+          </MypagePro2>
+        </MypageUserInfo>
+        {/* ---------------3---------------------------- */}
+        <MypageMenu>
+          <MypageMenuN>찜</MypageMenuN>
+          <MypageMenuN>리뷰</MypageMenuN>
+          <MypageMenuN>투표</MypageMenuN>
+        </MypageMenu>
+        {/* ---------------4---------------------------- */}
+        <MypageJjimStore>
+          <MypageJjimLine>
+            <MypageJjimUnit src="..\img\옥반1.jpg" />
+            <MypageJjimUnit src="../img/음식2.jpg" />
+            <MypageJjimUnit src="img/음식3.jpg" />
+          </MypageJjimLine>
+          <MypageJjimLine>
+            <MypageJjimUnit src="img/음식4.jpg" />
+            <MypageJjimUnit src="img/음식5.jpg" />
+            <MypageJjimUnit src="img/음식6.jpg" />
+          </MypageJjimLine>
+        </MypageJjimStore>
+      </MypageCon>
+    </div>
+  );
 };
 
 export default Mypage;
--- src/pages/Mypage/StyledMypage.jsx
+++ src/pages/Mypage/StyledMypage.jsx
@@ -1,71 +1,66 @@
 import styled, { css } from "styled-components";
 
-
 export const Test = styled.div`
-    color: red;
+  color: red;
 `;
 
 export const MypageCon = styled.div`
-    display: flex;
-    flex-direction :column;
-    max-width :1200px;
+  display: flex;
+  flex-direction: column;
+  max-width: 1200px;
 
-    justify-items :center;
+  justify-items: center;
 
-    margin:0 auto;
+  margin: 0 auto;
 `;
 
 export const LogoImg = styled.img`
-    height = "100%";
-    width = "100%";
-    margin-left :1vw;
+  height: "100%";
+  width: "100%";
+  margin-left: 1vw;
 `;
 
 export const ProfileImg = styled.img`
-    height = "auto";
-    width = "auto";
-    max-width = 100px;
-    background-size :contain;
-    border-radius : 12vw;
+  background-size: contain;
+  border-radius: 12vw;
 `;
 
 export const MypageTop = styled.div`
-    display :flex;
-    flex :9vw;
-    height :5vw;
-    max-height :7rem;
+  display: flex;
+  flex: 9vw;
+  height: 5vw;
+  max-height: 7rem;
 `;
 
-
 export const MypageUserInfo = styled.div`
-    display :flex;
-    height:20vw;
-    max-height :200px;
-    flex-direction :row;
-    margin-top :20px;
+  display: flex;
+  height: 20vw;
+  max-height: 200px;
+  flex-direction: row;
+  margin-top: 20px;
 `;
 
 export const MypagePro1 = styled.div`
-    width :20vw;
-    height :20vw;
-    max-width :200px;
-    max-height :200px;
-    border :2px solid black;
-    border-radius : 10vw;
+  width: 20vw;
+  height: 20vw;
+  max-width: 200px;
+  max-height: 200px;
+  border: 2px solid black;
+  border-radius: 10vw;
 `;
 
 export const MypagePro2 = styled.div`
-    flex :1;
-    display :flex;
-    flex-direction :column;
+  flex: 1;
+  display: flex;
+  flex-direction: column;
 
-    padding-left : 1em;
-    font-size :3vw;
+  padding-left: 1em;
+  font-size: 3vw;
 `;
 
 export const MypageProNickname = styled.div`
-    flex :1;
-    font-weight :bolder;
+  flex: 1;
+  font-weight: bolder;
 `;
 
 // .mypage_pro_nickname>p{
@@ -73,80 +68,76 @@ export const MypageProNickname = styled.div`
 // }
 
 export const MypageProEtc = styled.div`
-    flex :1;
-    font-size :2.5vw;
+  flex: 1;
+  font-size: 2.5vw;
 `;
 
 export const ChangeBtn = styled.button`
-    font-weight :600;
-    font-size :2vw;
-    margin-left :1.5rem;
-    padding :1.2vw;
-    padding-left :1.5vw;
-    padding-right :1.5vw;
-    border-radius :3vw;
-    background-color :#D3F6F3;
-    border-color :#3f928d;
+  font-weight: 600;
+  font-size: 2vw;
+  margin-left: 1.5rem;
+  padding: 1.2vw;
+  padding-left: 1.5vw;
+  padding-right: 1.5vw;
+  border-radius: 3vw;
+  background-color: #d3f6f3;
+  border-color: #3f928d;
 
-    &:active{
-        background-color :#ABD4D0;
-    }
+  &:active {
+    background-color: #abd4d0;
+  }
 `;
 
 export const MypageMenu = styled.div`
-    display :flex;
-    height :7vw;
-    max-height :80px;
-    flex-direction :row;
-    text-align :center;
-    align-items:center;
+  display: flex;
+  height: 7vw;
+  max-height: 80px;
+  flex-direction: row;
+  text-align: center;
+  align-items: center;
 
-    background-color :#FBD1B7;
-    color :white;
-    font-size :2.5vw;
-    margin-top :20px;
-    margin-bottom :1vw;
+  background-color: #fbd1b7;
+  color: white;
+  font-size: 2.5vw;
+  margin-top: 20px;
+  margin-bottom: 1vw;
 `;
 
 export const MypageMenuN = styled.div`
-    flex :1;
-    padding :1.6vw;
+  flex: 1;
+  padding: 1.6vw;
 
-    &:active{
-        border-bottom : 0.8vw solid white;
-    }
-    
+  &:active {
+    border-bottom: 0.8vw solid white;
+  }
 `;
 
 export const MypageJjimStore = styled.div`
-    display :flex;
-    flex :1;
-    flex-direction :column;
-    
-    margin-left :5vw;
-    margin-right :5vw;
-    background-color :bisque;
+  display: flex;
+  flex: 1;
+  flex-direction: column;
+
+  margin-left: 5vw;
+  margin-right: 5vw;
+  background-color: bisque;
 `;
 
 export const MypageJjimLine = styled.div`
-    display :flex;
-    flex :1;
-    flex-direction :row;
-    background-color :#dddddd;
+  display: flex;
+  flex: 1;
+  flex-direction: row;
+  background-color: #dddddd;
 `;
 
 export const MypageJjimUnit = styled.img`
-    flex :1;
-    
-    height :30vw;
-    border :1px solid rgb(116, 116, 116);
+  flex: 1;
 
-    max-width :350px;
-    max-height :350px;
+  height: 30vw;
+  border: 1px solid rgb(116, 116, 116);
+
+  max-width: 350px;
+  max-height: 350px;
 `;
-
-
-
 
 // .mypage_pro_etc>p{
 //     margin :0px;
@@ -169,5 +160,5 @@ export const MypageJjimUnit = styled.img`
 // 하늘 - #D3F6F3
 // 레몬 - #F9FCE1
 // 치즈 - #FEE9B2
-// 살구 - #FBD1B7 
+// 살구 - #FBD1B7
 // */
